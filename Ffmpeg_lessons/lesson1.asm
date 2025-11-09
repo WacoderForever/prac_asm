6c319d34c518c6bae750bd5314dd203ce96dccc2
WacoderForever <sethomondi088@gmail.com>
2025-11-09 17:56:46 +0300

    ffmpeg1

---
+++ Ffmpeg_lessons/lesson1.asm
@@ -0,0 +1,16 @@
+%include "x86inc.asm"
+
+SECTION .text
+
+; static void add(unint8 *src, const unint *src2)
+INIT_XMM sse2
+cglobal add_values 2, 2, 2, src, src2
+
+    movu m0,[srcq]
+    movu m1,[src2q]
+
+    paddb m0,m1
+
+    movu [srcq],m0
+
+    ret
